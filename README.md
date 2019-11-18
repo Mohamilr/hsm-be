e9c3a4292673872988b9ca0b1876b13e6207bbf1
Ephraimdev <aigbefoephraim@yahoo.com>
2019-11-18 20:23:33 +0100

    Set up HMS BE folder structure

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# hsm-be
+## Installing
+
+#### Prerequisites
+
+Ensure you have **NodeJS** installed by entering `node -v` on your terminal
+If you don't have **NodeJS** installed go to the [NodeJS Website](http://nodejs.org), and follow the download instructions
+
+To install this app
+
+```
+git clone https://github.com/EphraimDev/hsm-be.git
+```
+
+cd into the project folder and install the required dependencies
+
+```
+npm install
+```
+
+Create a `.env` file by running `cp .env.sample .env` on your terminal
+
+Update your `.env` file with the necessary informations
+
+Run server
+
+```
+npm run start:dev
---
+++ src/app.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+import express from 'express';
+import bodyParser from 'body-parser';
+import morganLogger from 'morgan';
+import cors from 'cors';
+
+import router from './routes';
+
+const app = express();
+
+app.use(morganLogger('dev'));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api/v1', router);
+
+app.get('/', (_req, res) => {
+  res.status(200).send('Welcome to HMS API');
+});
+
+const port = process.env.PORT || 4000;
+
+if (!module.parent) {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
+
+export default app;
