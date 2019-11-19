d69144505bf5290cfad8f91661d78b253796085e
mohammed <ibrahimdamy7@gmail.com>
2019-11-19 14:47:18 +0100

    chore(sign up): sign up a new user

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import login from '../controller/Login';
+import signUp from '../controller/signUp';
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/signup', signUp);
 
 export default router;
