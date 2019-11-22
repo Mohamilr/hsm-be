9705e0109577653748be0447b9346e74e609c204
codedcoderr <codedcoderrr@gmail.com>
2019-11-22 19:20:24 +0100

    separate profile route

---
+++ src/routes/Profile.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import profile from '../controller/Profile';
+import verify from '../controller/Verify';
+const router = express.Router();
+
+router.put('/:id', verify, profile);
+
+export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import auth from './Auth';
-import profile from '../controller/Profile';
-import verify from '../controller/Verify';
+import profile from './Profile';
 const router = express.Router();
 
 router.use('/auth', auth);
-router.put('/profile/:id', verify, profile);
+router.use('/profile', profile);
 
 export default router;
